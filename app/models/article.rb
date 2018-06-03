420b3b9e155e4c4641bfba6061107d6aa5c314d9
Darius Seals <tristarmediagroupinc@gmail.com>
2018-06-03 11:55:41 -0400

    added links to show and edit pages, also added links so that you can get back to the main page from any page

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,3 +2,5 @@ class Article < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300}
 end
+
+#Im pretty sure ActiveRecord::Base is supposed to be ApplicationRecord, but its working... most likely because ActiveRecord::Base is what ApplicationRecord inherits
