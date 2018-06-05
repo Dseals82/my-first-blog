29ce6c21dcc3c86d529ae2689d6c9a21d9267e7a
Darius Seals <tristarmediagroupinc@gmail.com>
2018-06-05 16:14:37 -0400

    added association between user and articles and added debugger view

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ActiveRecord::Base
   belongs_to :user
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300}
+  validates :user_id, presence: true
 end
 
 #Im pretty sure ActiveRecord::Base is supposed to be ApplicationRecord, but its working... most likely because ActiveRecord::Base is what ApplicationRecord inherits
