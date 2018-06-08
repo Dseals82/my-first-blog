7581dbe479cac82647de7dd192384f373b64de4b
Darius Seals <tristarmediagroupinc@gmail.com>
2018-06-08 10:19:50 -0400

    modified articles controller pages and users controller, there are still some issues like signed in user cannot successfully update or delete their articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
 
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -17,7 +19,7 @@ class ArticlesController < ApplicationController
   def create
 
     @article = Article.new(article_params)
-      @article.user = User.first
+      @article.user = current_user
       if @article.save
         flash[:success] = "Excellent! Your Article was successfully created!"
         redirect_to article_path(@article)
@@ -27,7 +29,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    # @article = Article.find(params[:id])
+
     if @article.update(article_params)
       flash[:success] = "Article was successfully updated!"
       redirect_to article_path(@article)
@@ -38,11 +40,10 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    # @article = Article.find(params[:id])
+
   end
 
   def destroy
-    # @article = Article.find(params[:id])
     @article.destroy
     flash[:danger] = "Article successfully deleted!"
     redirect_to articles_path
@@ -56,4 +57,10 @@ class ArticlesController < ApplicationController
     def article_params
         params.require(:article).permit(:title, :description)
     end
+
+    def require_same_user
+      if current_user !="Oops! You can only edit or delete your own articles!"
+        redirect_to root_path
+    end
+  end
 end
